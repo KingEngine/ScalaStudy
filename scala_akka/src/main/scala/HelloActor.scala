bb621f0c77b2a365779cbe59a182b95847f85af8
KingEngine <wangkechaodream@163.com>
2016-03-22 15:32:58 +0800

    Story 182: Fix benchmarks for speed

---
+++ scala_akka/src/main/scala/HelloActor.scala
@@ -0,0 +1,26 @@
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.Actor.Receive
+
+/**
+  * 弄着玩
+  *
+  * @author King
+  * @version 2016/3/22
+  */
+class HelloActor extends Actor {
+  override def receive = {
+    case "hello" => println("hello back to u")
+    case _ => println("huh?")
+  }
+}
+
+object Test1_HelloActor extends App {
+
+  val system = ActorSystem("HelloSystem")
+
+  val helloActor = system.actorOf(Props[HelloActor])
+
+  helloActor ! "hello"
+  helloActor ! "what"
+  system.shutdown
+}
